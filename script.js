60610fac4219fadf82bd4d27d23eb6bf7fa4286d
Estudante Alura <105920438+EmilyMeira@users.noreply.github.com>
2024-08-13 08:33:24 -0300

    a

--- script.js
+++ script.js
@@ -88,6 +88,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
@@ -99,13 +100,15 @@ function mostraAlternativas() {
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
-           atual++;
-           mostraPergunta()
-        })
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada())
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
 mostraPergunta();
 
